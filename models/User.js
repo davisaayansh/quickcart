ce699875f1104ef31c959556822c77ece53fd477
davisaayansh <107945536+davisaayansh@users.noreply.github.com>
2026-01-03 20:47:44 +0530

    chore: upgrade Next.js to fix CVE-2025-66478

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import { unique } from "next/dist/build/utils";
+// import { unique } from "next/dist/build/utils";
 
 const userSchema = new mongoose.Schema({
-    _id: { Type: String, required: true },
-    name: { Type: String, required: true },
-    email: { Type: String, required: true, unique: true },
-    imageUrl: { Type: String, required: true },
-    caetItems: { Type: Object, default: {} }
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} }
 }, {minimize: false})
 
 const User = mongoose.model.user || mongoose.model('user', userSchema)
